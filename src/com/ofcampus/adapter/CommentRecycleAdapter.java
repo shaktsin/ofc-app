a38ea37d489621ee56ca72bb0588c1e4b179fd6d
Dibakar <dibakar@ofcampus.com>
2015-01-22 01:28:42 +0530

    modify

---
+++ src/com/ofcampus/adapter/CommentRecycleAdapter.java
@@ -0,0 +1,144 @@
+package com.ofcampus.adapter;
+
+import java.util.ArrayList;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.ofcampus.R;
+import com.ofcampus.model.JobDetails;
+
+public class CommentRecycleAdapter extends BaseAdapter{
+
+	private LayoutInflater inflater;
+	private Context mContext;
+	private ArrayList<JobDetails> arraJobComment;
+	
+	private ImageLoader imageLoader=ImageLoader.getInstance();
+	private DisplayImageOptions options;
+	
+	public CommentRecycleAdapter(Context context,ArrayList<JobDetails> arraJobComment_){
+		this.mContext=context;
+		this.arraJobComment=arraJobComment_;
+		this.inflater=LayoutInflater.from(mContext);
+		
+		options = new DisplayImageOptions.Builder()
+				.showImageOnLoading(R.drawable.ic_profilepic)
+				.showImageForEmptyUri(R.drawable.ic_profilepic)
+				.showImageOnFail(R.drawable.ic_profilepic).cacheInMemory(true)
+				.cacheOnDisk(true).considerExifParams(true).build();
+		imageLoader.init(ImageLoaderConfiguration.createDefault(context));
+		
+	}
+
+	public void refreshView(ArrayList<JobDetails> arraJobComment_) { 
+		this.arraJobComment=arraJobComment_;
+		notifyDataSetChanged();
+	}
+	
+	public void refreshView(JobDetails mJobDetails) { 
+		this.arraJobComment.add(mJobDetails); 
+		notifyDataSetChanged();
+	}
+
+	@Override
+	public int getCount() {
+		return arraJobComment.size();
+	}
+
+	@Override
+	public Object getItem(int position) {
+		return null;
+	}
+
+	@Override
+	public long getItemId(int position) {
+		return 0;
+	}
+
+
+
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		
+		ViewHolder mHolder;
+		if (convertView==null) {
+			mHolder=new ViewHolder();
+			
+			convertView=inflater.inflate(R.layout.inflate_commentlist_row, parent,false);
+			
+			mHolder.img_prfpic=(ImageView)convertView.findViewById(R.id.joblistview_img_pic);
+			mHolder.img_arrow=(ImageView)convertView.findViewById(R.id.joblistview_img_arrow);
+			mHolder.txt_name=(TextView)convertView.findViewById(R.id.joblistview_txt_name);
+			mHolder.txt_date=(TextView)convertView.findViewById(R.id.joblistview_txt_postdate);
+			mHolder.txt_subject=(TextView)convertView.findViewById(R.id.joblistview_txt_subject);
+			mHolder.txt_jobdetails=(TextView)convertView.findViewById(R.id.joblistview_txt_contain);
+			mHolder.linear_buttonsection=(LinearLayout)convertView.findViewById(R.id.joblistview_linear_buttonsection); 
+					
+			mHolder.rel_details=(RelativeLayout)convertView.findViewById(R.id.inflate_joblistview_rel); 
+			mHolder.rel_comment=(RelativeLayout)convertView.findViewById(R.id.inflate_comment_rel); 
+			
+			mHolder.img_commentprfpic=(ImageView)convertView.findViewById(R.id.inflate_comment_img_pic);
+			mHolder.txt_commentname=(TextView)convertView.findViewById(R.id.inflate_comment_txt_name);
+			mHolder.txt_commentdate=(TextView)convertView.findViewById(R.id.inflate_comment_txt_name);
+			mHolder.txt_commenteddetails=(TextView)convertView.findViewById(R.id.inflate_comment_txt_contain);
+			
+			convertView.setTag(mHolder);
+		}else{
+			mHolder = (ViewHolder) convertView.getTag();
+		}
+		
+		final JobDetails mJobDetails = arraJobComment.get(position);
+		
+		if (position==0) {
+			mHolder.rel_details.setVisibility(View.VISIBLE);
+			mHolder.rel_comment.setVisibility(View.GONE);
+			mHolder.linear_buttonsection.setVisibility(View.GONE);
+			if (mJobDetails!=null) {
+				String url=mJobDetails.getImage();
+				if (url!=null && !url.equals("") && !url.equals("null")) {
+					imageLoader.displayImage(url, mHolder.img_prfpic, options);
+				}
+				mHolder.txt_name.setText(mJobDetails.getName());
+				mHolder.txt_date.setText("Posted on "+mJobDetails.getPostedon());
+				mHolder.txt_subject.setText(mJobDetails.getSubject());
+				mHolder.txt_jobdetails.setText(mJobDetails.getContent());
+				mHolder.img_arrow.setVisibility(View.GONE);
+			}
+		}else {
+			mHolder.rel_details.setVisibility(View.GONE);
+			mHolder.rel_comment.setVisibility(View.VISIBLE);
+			
+			
+			String url=mJobDetails.getImage();
+			if (url!=null && !url.equals("") && !url.equals("null")) {
+				imageLoader.displayImage(url, mHolder.img_commentprfpic, options);
+			}
+			mHolder.txt_commentname.setText(mJobDetails.getName());
+			mHolder.txt_commentdate.setText("Commented on "+mJobDetails.getPostedon());
+			mHolder.txt_commenteddetails.setText(mJobDetails.getContent());
+		}
+		
+		return convertView;
+	}
+	
+	private class ViewHolder {
+		public ImageView img_prfpic, img_arrow,img_commentprfpic;
+		public TextView txt_name, txt_date, txt_subject, txt_jobdetails,txt_commentname,txt_commentdate,txt_commenteddetails;
+		public LinearLayout linear_buttonsection;
+		public RelativeLayout rel_details,rel_comment;
+	}
+
+	
+}
---
+++ src/com/ofcampus/parser/CommentParser.java
@@ -0,0 +1,229 @@
+package com.ofcampus.parser;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.os.Build;
+
+import com.ofcampus.Util;
+import com.ofcampus.model.JobDetails;
+
+public class CommentParser {
+
+	private Context mContext;
+	private String STATUS="status";
+	private String RESULTS="results";
+//	private String EXCEPTION="exception";
+//	private String MESSAGES="messages";
+
+
+	/*Job List Key*/
+	private String POSTID="postId";
+	private String SUBJECT="subject";
+	private String ISB_JOBS="ISB JOBS";
+	private String CONTENT="content";
+	private String POSTEDON="postedOn";
+	private String USERDTO="userDto";
+	private String ID="id";
+	private String NAME="name";
+	private String IMAGE="image";
+	private String REPLYDTO="replyDto";
+	private String SHAREDTO="shareDto";
+	
+	/**CommentList Key*/
+	private String COMMENTLISTRESPONSE="commentListResponse";
+
+	/*Response JSON key value*/
+	private String responsecode="";
+	private String responseDetails="";
+	
+	
+	
+	
+	
+	public void parse(Context context,String id,String auth) {  
+		this.mContext = context;
+		List<NameValuePair> postData = getparamBody(auth);
+		
+		JobDetailsAsyncAsync mJobDetailsAsyncAsync = new JobDetailsAsyncAsync(mContext,postData,id); 
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+			mJobDetailsAsyncAsync.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+		} else {
+			mJobDetailsAsyncAsync.execute();  
+		}
+	}
+	
+	
+	private class JobDetailsAsyncAsync extends AsyncTask<Void, Void, Void>{
+		private Context context;
+		private String authenticationJson;
+		private boolean isTimeOut=false;
+		private ArrayList<JobDetails> arrayJobsComment;
+		private ProgressDialog mDialog;
+		private List<NameValuePair> postData;
+		private String id="";
+		
+		public JobDetailsAsyncAsync(Context mContext, List<NameValuePair> postData_, String id_) {
+			this.context = mContext;
+			this.postData = postData_; 
+			this.id=id_; 
+		}
+
+		@Override
+		protected void onPreExecute() {
+			super.onPreExecute();
+			mDialog=new ProgressDialog(mContext);
+			mDialog.setMessage("Loading...");
+			mDialog.setCancelable(false);
+			mDialog.show();
+		}
+
+		@Override
+		protected Void doInBackground(Void... params) {
+			
+			try {
+				
+				String[] responsedata =	Util.GetRequest(postData,Util.getJobDetailsUrl(id));
+				authenticationJson = responsedata[1];
+				isTimeOut = (responsedata[0].equals("205"))?true:false;
+				
+				if (authenticationJson!=null && !authenticationJson.equals("")) {
+					JSONObject mObject=new JSONObject(authenticationJson);
+					responsecode = Util.getJsonValue(mObject, STATUS);
+					if (responsecode!=null && responsecode.equals("200")) {
+						JSONObject Obj = mObject.getJSONObject(RESULTS);
+						if (Obj!=null) {
+							arrayJobsComment = getParseData(Obj);
+						}
+					}else if(responsecode!=null && responsecode.equals("500")){
+						JSONObject userObj = mObject.getJSONObject(RESULTS);
+						if (userObj!=null) {
+							responseDetails=userObj.getJSONArray("messages").get(0).toString();
+						}
+					}
+					
+				}
+			} catch (JSONException e) {
+				e.printStackTrace();
+			}
+			return null;
+		}
+
+		@Override
+		protected void onPostExecute(Void result) {
+			super.onPostExecute(result);
+			
+			if (mDialog!=null && mDialog.isShowing()) {
+				mDialog.cancel();
+				mDialog=null;
+			}
+			
+			if (isTimeOut) {
+				if (commentparserinterface != null) { 
+					commentparserinterface.OnError(); 
+				}
+			}else if (responsecode.equals("200")) {
+				if (arrayJobsComment!=null) {
+					if (commentparserinterface!=null) {
+						commentparserinterface.OnSuccess(arrayJobsComment); 
+					}
+				}else {
+					Util.ShowToast(mContext, "Job details parser error.");
+					if (commentparserinterface != null) {
+						commentparserinterface.OnError(); 
+					}
+				}
+			}else if (responsecode.equals("500")){
+				Util.ShowToast(mContext, responseDetails);
+			}else {
+				Util.ShowToast(mContext, "Job details parser error.");
+			}
+		}
+	}
+	
+	
+	private ArrayList<JobDetails> getParseData(JSONObject jsonobject){
+
+
+		ArrayList<JobDetails> arrayJobsComment = null; 
+		try {
+			arrayJobsComment=new ArrayList<JobDetails>();
+			JobDetails mJobDetails  =new JobDetails();
+			mJobDetails.setPostid(Util.getJsonValue(jsonobject, POSTID)); 
+			mJobDetails.setSubject(Util.getJsonValue(jsonobject, SUBJECT));
+			mJobDetails.setIsb_jobs(Util.getJsonValue(jsonobject, ISB_JOBS));
+			mJobDetails.setContent(Util.getJsonValue(jsonobject, CONTENT));
+			mJobDetails.setPostedon(Util.getJsonValue(jsonobject, POSTEDON));
+			
+			JSONObject userJSONobj=jsonobject.getJSONObject(USERDTO);
+			mJobDetails.setId(Util.getJsonValue(userJSONobj, ID));
+			mJobDetails.setName(Util.getJsonValue(userJSONobj, NAME));
+			mJobDetails.setImage(Util.getJsonValue(userJSONobj, IMAGE));
+			
+			mJobDetails.setReplydto(Util.getJsonValue(jsonobject, REPLYDTO));
+			mJobDetails.setSharedto(Util.getJsonValue(jsonobject, SHAREDTO));
+			
+			arrayJobsComment.add(mJobDetails);
+			
+			try {
+				JSONArray commentArrJsonArray=jsonobject.getJSONArray(COMMENTLISTRESPONSE);
+				
+				if (commentArrJsonArray!=null && commentArrJsonArray.length()>=1) {
+					for (int i = 0; i < commentArrJsonArray.length(); i++) {
+						
+						JobDetails jobComment=new JobDetails();
+						jobComment.setPostid(Util.getJsonValue(jsonobject, POSTID)); 
+						jobComment.setContent(Util.getJsonValue(jsonobject, CONTENT));
+						
+						JSONObject jobCommentuserJSONobj=jsonobject.getJSONObject(USERDTO);
+						jobComment.setId(Util.getJsonValue(jobCommentuserJSONobj, ID));
+						jobComment.setName(Util.getJsonValue(jobCommentuserJSONobj, NAME));
+						jobComment.setImage(Util.getJsonValue(jobCommentuserJSONobj, IMAGE));
+						arrayJobsComment.add(jobComment);
+					}
+				}
+				
+			} catch (JSONException e) {
+				e.printStackTrace();
+			}
+			
+			
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		return arrayJobsComment;
+		
+	}
+	
+	public static List<NameValuePair> getparamBody(String authorization) {
+		List<NameValuePair> pairsofEducation = new ArrayList<NameValuePair>();
+		pairsofEducation.add(new BasicNameValuePair("Authorization", authorization));
+		return pairsofEducation;
+	}
+	
+	public CommentParserInterface commentparserinterface;
+
+	public CommentParserInterface getCommentparserinterface() {
+		return commentparserinterface;
+	}
+
+	public void setCommentparserinterface(
+			CommentParserInterface commentparserinterface) {
+		this.commentparserinterface = commentparserinterface;
+	}
+
+	public interface CommentParserInterface {
+		public void OnSuccess(ArrayList<JobDetails> arrayJobsComment); 
+
+		public void OnError();
+	}
+}
